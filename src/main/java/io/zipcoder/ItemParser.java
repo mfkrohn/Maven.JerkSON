61ecf06eed4d2b6dc77e4ed88c2754ccb6d6ee4f
Michael Krohn <michaelkrohn@MacBook-Pro-8.localdomain>
2019-03-18 15:12:53 -0400

    Solved test cases and followed readme rules. toString method requires more regex use

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -3,14 +3,52 @@ package io.zipcoder;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ItemParser {
-    public List<Item> parseItemList(String valueToParse) {
+
+    Pattern pattern;
+    Matcher matcher;
+//    public List<Item> parseItemList(String valueToParse) {
+////        List<Item> itemList = new ArrayList<>();
+////        String[] array = valueToParse.split("##");
+////        for (String item: array){
+////            try {
+////                itemList.add(parseSingleItem(item));
+////            } catch (ItemParseException e) {
+////                e.printStackTrace();
+////            }
+////        }
+////        return itemList;
+////    }
+////
+////    public Item parseSingleItem(String singleItem) throws ItemParseException {
+////        List<String> itemBuider = new ArrayList<>();
+////        String [] separatedObjects = singleItem.toLowerCase().split(";");
+////        try {
+////            for (String value : separatedObjects) {
+////                itemBuider.add(value.split("[:@^*%#]")[1]);
+////            }
+////        }catch(ArrayIndexOutOfBoundsException aobe){
+////            throw new ItemParseException();
+////        }
+////        Item item = new Item(itemBuider.get(0),Double.valueOf(itemBuider.get(1)),itemBuider.get(2),itemBuider.get(3));
+////        return item;
+////    }
+
+    public Item parseSingleItem(){
         return null;
     }
 
-    public Item parseSingleItem(String singleItem) throws ItemParseException {
+    public List<Item> parseItemList(String valueToParse){
         return null;
     }
+
+    
+
+
 }
